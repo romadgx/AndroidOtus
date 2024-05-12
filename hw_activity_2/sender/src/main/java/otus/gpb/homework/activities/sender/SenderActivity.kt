a52e3f4d103c06c6b67a93b0d1af98d91271b716
RomanDGRB <romanruby@yandex.ru>
2024-05-12 17:20:45 +0300

    выполнил два задания

---
+++ hw_activity_2/sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,45 @@
+package otus.gpb.homework.activities.sender
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SenderActivity : AppCompatActivity() {
+
+    private lateinit var btnOpenMap: Button
+    private lateinit var btnSendEmail: Button
+    private lateinit var btnOpenReceiver: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sender)
+
+        btnOpenMap = findViewById(R.id.btn_maps)
+        btnSendEmail = findViewById(R.id.btn_mail)
+        btnOpenReceiver = findViewById(R.id.btn_open_receiver)
+
+        btnOpenMap.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=Рестораны") // не смог понять как получить "текущую локацию"
+            ).apply {
+                setPackage("com.google.android.apps.maps")
+            }
+            startActivity(intent)
+        }
+
+        btnSendEmail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                setType("message/rfc822")
+                setData(Uri.parse("mailto:"))
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("android@otus.ru"))
+                putExtra(Intent.EXTRA_SUBJECT,"Hello!")
+                putExtra(Intent.EXTRA_TEXT, "Hello Otus!")
+            }
+            startActivity(intent)
+        }
+
+    }
+}
